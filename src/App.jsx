36be42b8b4e8cdb7a6348e06151163a99bb8e60e
JatinDohale <jatindohale2004@gmail.com>
2025-08-04 17:03:40 +0530

    basic Home import change

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 // App.js
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './home/home';
+import Home from './home/Home';
 import Meal from './meal/Meal';
 
 function App() {
